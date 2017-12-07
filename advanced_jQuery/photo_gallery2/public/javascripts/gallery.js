6457190fdc1d3c8af804de6c8fc5f430fce150e5
John Vaughn <johnjvaughn@gmail.com>
2017-12-07 16:47:26 -0500

    Photo Gallery finished

--- advanced_jQuery/photo_gallery2/public/javascripts/gallery.js
+++ advanced_jQuery/photo_gallery2/public/javascripts/gallery.js
@@ -1,7 +1,7 @@
 $(function() {
   var templates = {};
   var $elements = {};
-  var photo_info_arr;
+  var photo_info_arr = [];
   var photo_index = 0;
 
   function collectJQElements() {
@@ -23,20 +23,30 @@ $(function() {
     });
   }
 
-  var addPhoto = function() {
+  function addPhoto() {
     $elements.photos.html(templates.photos({photos: photo_info_arr}));
-  };
+  }
 
   function addPhotoInfo() {
     $elements.photo_info.html(templates.photo_information(photo_info_arr[photo_index]));
   }
 
-  var addComments = function(comments_info_arr) {
+  function addComments(comments_info_arr) {
     $elements.comments.html(templates.comments({comments: comments_info_arr}));
   }
 
   var slideshow = {
-    changePhoto: function(e) {
+    displayCurrentSlide: function() {
+      var $current_fig = $('#slides figure:visible');
+      var $new_fig = $current_fig.parent().children('figure').eq(photo_index);
+      $current_fig.fadeOut();
+      $new_fig.fadeIn();
+      $elements.photo_info.html(templates.photo_information(photo_info_arr[photo_index]));
+      getComments(photo_info_arr[photo_index].id);
+      $('input[name=photo_id]').val(photo_info_arr[photo_index].id);
+      console.log($('input[name=photo_id]'));
+    },
+    changeSlide: function(e) {
       e.preventDefault();
       if ($(e.target).hasClass('next')) {
         photo_index = (photo_index + 1) % photo_info_arr.length;
@@ -46,28 +56,69 @@ $(function() {
       } else {
         return;
       }
-      var $current_fig = $('#slides figure:visible');
-      var $new_fig = $current_fig.parent().children('figure').eq(photo_index);
-      $current_fig.fadeOut();
-      $new_fig.fadeIn();
-      $elements.photo_info.html(templates.photo_information(photo_info_arr[photo_index]));
-      getComments(photo_info_arr[photo_index].id)
+      this.displayCurrentSlide();
     },
     bindEvents: function() {
-      $elements.slideshow.on('click', 'a', changePhoto);
+      $elements.slideshow.on('click', 'a', this.changeSlide.bind(this));
+    },
+    init: function() {
+      this.bindEvents();
+    },
+  };
+
+  var actions = {
+    incrementAction: function(e) {
+      e.preventDefault();
+      var $link = $(e.target);
+      $.ajax({
+        url: $link.attr("href"),
+        data: { photo_id: $link.data("id") },
+        type: "POST",
+        dataType: "json",
+      }).done(function(json) {
+        $link.text(function(i, text) {
+          return text.replace(/\d+/, json.total);
+        });
+        photo_info_arr[photo_index][$link.data("property")] = json.total;
+      }).fail(ajaxError);
+    },
+    bindEvents: function() {
+      $elements.photo_info.on('click', '.actions .button', this.incrementAction.bind(this));
     },
     init: function() {
       this.bindEvents();
     },
   }
 
-  var ajaxError = function( xhr, status, errorThrown ) {
-    alert( "AJAX: Sorry, there was a problem!" );
-    console.log( "Error: " + errorThrown );
-    console.log( "Status: " + status );
-    console.dir( xhr );
-  };
+  var comments_form = {
+    submitForm: function(e) {
+      e.preventDefault();
+      $form = $(this);
+      $.ajax({
+        url: $form.attr("action"),
+        data: $form.serialize(),
+        type: $form.attr("method"),
+        dataType: "json",
+      }).done(function(json) {
+        $elements.comments.append(templates.comment(json));
+        $form.find('input, textarea').val('');
+      }).fail(ajaxError);
+    },
+    bindEvents: function() {
+      $('#comments form').on('submit', this.submitForm);
+    },
+    init: function() {
+      this.bindEvents();
+    },
+  }
 
+  function ajaxError(xhr, status, errorThrown) {
+    alert( "AJAX: Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr );
+  }
+  
   function getComments(id) {
     $.ajax({
       url: "/comments",
@@ -85,12 +136,18 @@ $(function() {
       addPhoto();
       addPhotoInfo();
       getComments(photo_info_arr[photo_index].id);
-      slideshow.init();
     }).fail(ajaxError);
+  }
+
+  function initializeEvents() {
+    slideshow.init();
+    actions.init();
+    comments_form.init();
   }
 
   collectJQElements();
   collectTemplatesPartials();
   getPhotos();
+  initializeEvents();
   
 });
